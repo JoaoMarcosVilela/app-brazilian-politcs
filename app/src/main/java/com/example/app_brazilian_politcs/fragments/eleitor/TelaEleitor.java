7a8f59b79eb75e6718a1821d2713b20c3ffafb16
JoaoMarcosVilela <joaosemp12345@gmail.com>
2023-11-21 20:27:42 -0300

    Hora na tela do eleitor

--- app/src/main/java/com/example/app_brazilian_politcs/fragments/eleitor/TelaEleitor.java
+++ app/src/main/java/com/example/app_brazilian_politcs/fragments/eleitor/TelaEleitor.java
@@ -4,63 +4,48 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.app_brazilian_politcs.R;
+import com.example.app_brazilian_politcs.databinding.FragmentTelaEleitorBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TelaEleitor#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TelaEleitor extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentTelaEleitorBinding binding;
+    private int i = 0;
 
     public TelaEleitor() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TelaEleitor.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TelaEleitor newInstance(String param1, String param2) {
-        TelaEleitor fragment = new TelaEleitor();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tela_eleitor, container, false);
+        binding = FragmentTelaEleitorBinding.inflate(inflater,container,false);
+
+
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            public void run() {
+                Date dataHoraAtual = new Date();
+                String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+                binding.textViewHora.setText(hora);
+                handler.postDelayed(this, 10*100);
+            }
+        });
+
+        return binding.getRoot();
+
     }
 }
